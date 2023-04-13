1363d38340e37ffcb062a1267199f9a86c0920c1
Ramesh <rameshdussa07@gmail.com>
2023-04-13 23:31:55 +0530

    done half of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,60 @@
-
+import { useState } from "react";
 
 export default function App() {
+  const [newTask, setNewTask] = useState("");
+  const [todos, setTodos] = useState([]);
 
-  return(
-    <form className="form-container">
-      <div className="rowOfForm">
-        <label htmlFor="item">New Task</label>
-        <input type="text" id="item" placeholder="add task here...."/>
-      </div>
-    </form>
-  )
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setNewTask(currentTask => {
+      return [
+        ...currentTask,
+        {
+          id: crypto.randomUUID(),
+          title: newTask,
+          completed: false,
+        },
+      ]
+    })
+
+    setNewTask("");
+  }
+
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Task</label>
+          <input
+            type="text"
+            id="item"
+            placeholder="add task here...."
+            value={newTask}
+            onChange={(e) => {
+              setNewTask(e.target.value);
+            }}
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+
+      <ul className="list">
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input type="checkbox" checked={todo.completed} />
+                {todo.title}
+              </label>
+              <button className="btn btn-danger">Delete</button>
+            </li>
+          )
+        })}
+      </ul>
+
+    </>
+  );
 }
-
-
